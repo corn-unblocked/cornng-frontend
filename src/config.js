ee9883d2cc6c558cd6a7f22ffd2bdb621685461a
vaughanm <vaughanm1@protonmail.com>
2024-08-11 00:30:54 -0500

    accidentally leaked private proxies oopsies

--- src/config.js
+++ src/config.js
@@ -1,20 +1,14 @@
 const wispProxyUrls = {
     "custom": "Custom",
-    "wss://aluu.xyz/wisp/": "Alu 1",
-    "wss://incog.works/wisp/": "Incognito 1",
-    "wss://math.mathpuns.lol/wisp/": "Incognito 2",
-    "wss://math.americahistory.online/wisp/": "Incognito 3",
-    "wss://english.geniuslecture.club/wisp": "Incognito 4",
+    "wss://aluu.xyz/wisp/": "Alu",
+    "wss://incog.works/wisp/": "Incognito",
     "wss://wisp.mercurywork.shop/": "Mercury",
 }
 
 const bareProxyUrls = {
     "custom": "Custom",
-    "https://aluu.xyz/bare/": "Alu 1",
-    "https://incog.works/bare/": "Incognito 1",
-    "https://math.mathpuns.lol/bare/": "Incognito 2",
-    "https://math.americahistory.online/bare/": "Incognito 3",
-    "https://english.geniuslecture.club/bare/": "Incognito 4",
+    "https://aluu.xyz/bare/": "Alu",
+    "https://incog.works/bare/": "Incognito",
 }
 
 const config = {
