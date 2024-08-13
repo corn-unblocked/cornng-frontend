a7f44e24bb43d7f6aa1ae9deff969a0610d7d595
vaughanm <vaughanm1@protonmail.com>
2024-08-12 19:28:20 -0500

    maybe not

--- src/uv/uv.config.js
+++ src/uv/uv.config.js
@@ -1,22 +1,22 @@
 function initUvConfig() {
-    let path = location.pathname;
+    let path;
     try {
-        path = window.location.pathname;
+        path = window.location.pathname + "/uv";
     } catch (_) {
         path = self.location.pathname;
     }
     let loc = path.substring(0, path.lastIndexOf("/"));
 
     self.__uv$config = {
-        prefix: loc + "/uv/service/",
+        prefix: loc + "/service/",
         encodeUrl: Ultraviolet.codec.xor.encode,
         decodeUrl: Ultraviolet.codec.xor.decode,
-        handler: loc + "/uv/uv.handler.js",
-        client: loc + "/uv/uv.client.js",
-        bundle: loc + "/uv/uv.bundle.js",
-        config: loc + "/uv/uv.config.js",
-        sw: loc + "/uv/uv.sw.js",
-        stockSW: loc + "/uv/sw.js",
+        handler: loc + "/uv.handler.js",
+        client: loc + "/uv.client.js",
+        bundle: loc + "/uv.bundle.js",
+        config: loc + "/uv.config.js",
+        sw: loc + "/uv.sw.js",
+        stockSW: loc + "/sw.js",
         loc: loc,
     };
 }
