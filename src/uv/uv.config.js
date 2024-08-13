5577865e5a2f87091b56b75afaacf1146495199a
vaughanm <vaughanm1@protonmail.com>
2024-08-12 19:38:14 -0500

    it was not the final fix

--- src/uv/uv.config.js
+++ src/uv/uv.config.js
@@ -3,22 +3,23 @@ function initUvConfig() {
     // script needs to run both in sw and normal js
     try {
         let path = window.location.pathname;
-        loc = path.substring(0, path.lastIndexOf("/")) + "/uv";
+        loc = path.substring(0, path.lastIndexOf("/"));
     } catch (_) {
         let path = self.location.pathname;
-        loc = path.substring(0, path.lastIndexOf("/"));
+        // this also includes /uv/ ending so cut that off as well
+        loc = path.substring(0, path.lastIndexOf("/", path.lastIndexOf("/")-1)));
     }
 
     self.__uv$config = {
-        prefix: loc + "/service/",
+        prefix: loc + "/uv/service/",
         encodeUrl: Ultraviolet.codec.xor.encode,
         decodeUrl: Ultraviolet.codec.xor.decode,
-        handler: loc + "/uv.handler.js",
-        client: loc + "/uv.client.js",
-        bundle: loc + "/uv.bundle.js",
-        config: loc + "/uv.config.js",
-        sw: loc + "/uv.sw.js",
-        stockSW: loc + "/sw.js",
+        handler: loc + "/uv/uv.handler.js",
+        client: loc + "/uv/uv.client.js",
+        bundle: loc + "/uv/uv.bundle.js",
+        config: loc + "/uv/uv.config.js",
+        sw: loc + "/uv/uv.sw.js",
+        stockSW: loc + "/uv/sw.js",
         loc: loc,
     };
 }
