c03750f0cf85edd1a990b47689c20654343175ba
vaughanm <vaughanm1@protonmail.com>
2024-08-14 22:49:59 -0500

    rework uvconfig file

--- src/config.js
+++ src/config.js
@@ -1,18 +1,18 @@
 const wispProxyUrls = {
-    "custom": "Custom",
+    custom: "Custom",
     "wss://aluu.xyz/wisp/": "Alu",
     "wss://incog.works/wisp/": "Incognito",
     "wss://definitelyscience.com/wisp/": "Definitely Science",
     "wss://wisp.mercurywork.shop/": "Mercury",
-}
+};
 
 const bareProxyUrls = {
-    "custom": "Custom",
+    custom: "Custom",
     "https://aluu.xyz/bare/": "Alu",
     "https://definitelyscience.com/bare/": "Definitely Science",
     "https://incog.works/bare/": "Incognito",
     "https://kazwire.com/bare/": "Kazwire",
-}
+};
 
 const config = {
     useBare: false,
@@ -20,7 +20,7 @@ const config = {
     wispCustomProxy: "",
     bareProxyIndex: 1,
     bareCustomProxy: "",
-}
+};
 
 function loadConfig() {
     let tmp = JSON.parse(localStorage.getItem("config"));
--- src/uv/uv.config.js
+++ src/uv/uv.config.js
@@ -1,16 +1,26 @@
-let __cornng$loc = self.location.pathname.includes("/uv/")
-    ? self.location.pathname.substring(0, self.location.pathname.indexOf("/uv/"))
-    : self.location.pathname.substring(0, self.location.pathname.lastIndexOf("/"));
+(() => {
+    let uvPfx = "/uv/";
+    // check if config is loaded in context of service worker or not
+    let loc = self.location.pathname.includes(uvPfx)
+        ? self.location.pathname.substring(
+              0,
+              self.location.pathname.indexOf(uvPfx),
+          )
+        : self.location.pathname.substring(
+              0,
+              self.location.pathname.lastIndexOf("/"),
+          );
 
-self.__uv$config = {
-    prefix: __cornng$loc + "/uv/service/",
-    encodeUrl: Ultraviolet.codec.xor.encode,
-    decodeUrl: Ultraviolet.codec.xor.decode,
-    handler: __cornng$loc + "/uv/uv.handler.js",
-    client: __cornng$loc + "/uv/uv.client.js",
-    bundle: __cornng$loc + "/uv/uv.bundle.js",
-    config: __cornng$loc + "/uv/uv.config.js",
-    sw: __cornng$loc + "/uv/uv.sw.js",
-    stockSW: __cornng$loc + "/uv/sw.js",
-    loc: __cornng$loc,
-};
+    self.__uv$config = {
+        prefix: loc + uvPfx + "service/",
+        encodeUrl: Ultraviolet.codec.xor.encode,
+        decodeUrl: Ultraviolet.codec.xor.decode,
+        handler: loc + uvPfx + "uv.handler.js",
+        client: loc + uvPfx + "uv.client.js",
+        bundle: loc + uvPfx + "uv.bundle.js",
+        config: loc + uvPfx + "uv.config.js",
+        sw: loc + uvPfx + "uv.sw.js",
+        stockSW: loc + uvPfx + "sw.js",
+        loc: loc,
+    };
+})();
